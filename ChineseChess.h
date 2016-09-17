bcd3c96bb25f7f145483dd5c6dbe295000c43d52
zhy755788055 <424211755@163.com>
2016-09-17 22:27:26 +0800

    改名字

--- ChineseChess.h
+++ ChineseChess.h
@@ -12,7 +12,7 @@
 #include "resource.h"		// main symbols
 
 /////////////////////////////////////////////////////////////////////////////
-// CDuanApp:
+// CChineseChessApp:
 // See duan.cpp for the implementation of this class
 //
 
@@ -23,24 +23,17 @@ public:
 
 // Overrides
 	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CDuanApp)
+	//{{AFX_VIRTUAL(CChineseChessApp)
 	public:
 	virtual BOOL InitInstance();
 	//}}AFX_VIRTUAL
 
 // Implementation
 
-	//{{AFX_MSG(CDuanApp)
+	//{{AFX_MSG(CChineseChessApp)
 		// NOTE - the ClassWizard will add and remove member functions here.
 		//    DO NOT EDIT what you see in these blocks of generated code !
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
 #endif
